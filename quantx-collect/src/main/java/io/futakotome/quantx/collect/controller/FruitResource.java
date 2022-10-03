dc5404a996a3194160c49d3838503e0907a4e376
futakotome <v-zzz.gd@chinatelecom.cn>
2022-10-03 20:33:15 +0800

    完善例子

--- quantx-collect/src/main/java/io/futakotome/quantx/collect/controller/FruitResource.java
+++ quantx-collect/src/main/java/io/futakotome/quantx/collect/controller/FruitResource.java
@@ -1,7 +1,10 @@
 package io.futakotome.quantx.collect.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import io.futakotome.quantx.collect.domain.Fruit;
 import org.jboss.logging.Logger;
+import org.jboss.resteasy.reactive.RestQuery;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
@@ -9,6 +12,9 @@ import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
 import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+import java.util.List;
 
 @ApplicationScoped
 @Produces("application/json")
@@ -88,8 +94,39 @@ public class FruitResource {
         return delete(id);
     }
 
-    private Response delete(Integer id) {
+    @GET
+    @Path("fruitsFindBy")
+    public Response findByDefault(@RestQuery String type, @RestQuery String value) {
+        return findBy(type, value);
+    }
 
+    @GET
+    @Path("{tenant}/fruitsFindBy")
+    public Response findByTenant(@RestQuery String type, @RestQuery String value) {
+        return findBy(type, value);
+    }
+
+    private Response findBy(String type, String value) {
+        if (!"name".equalsIgnoreCase(type)) {
+            throw new IllegalArgumentException("Currently only 'fruitsFindBy?type=name' is supported");
+        }
+        List<Fruit> list = entityManager.createNamedQuery("Fruits.findByName", Fruit.class)
+                .setParameter("name", value).getResultList();
+        if (list.size() == 0) {
+            return Response.status(404).build();
+        }
+        Fruit fruit = list.get(0);
+        return Response.status(200).entity(fruit).build();
+    }
+
+    private Response delete(Integer id) {
+        Fruit fruit = entityManager.getReference(Fruit.class, id);
+        if (fruit == null) {
+            throw new WebApplicationException("Fruit with id of " + id + " does not exist.", 404);
+        }
+        LOGGER.infov("delete #{0} {1}", fruit.getId(), fruit.getName());
+        entityManager.remove(fruit);
+        return Response.status(204).build();
     }
 
     private Fruit update(Integer id, Fruit fruit) {
@@ -126,5 +163,29 @@ public class FruitResource {
     private Fruit[] get() {
         return entityManager.createNamedQuery("Fruits.findAll", Fruit.class)
                 .getResultList().toArray(new Fruit[0]);
+    }
+
+    @Provider
+    public static class ErrorMapper implements ExceptionMapper<Exception> {
+        @Inject
+        ObjectMapper objectMapper;
+
+        @Override
+        public Response toResponse(Exception e) {
+            LOGGER.error("Failed to handle request", e);
+            int code = 500;
+            if (e instanceof WebApplicationException) {
+                code = ((WebApplicationException) e).getResponse().getStatus();
+            }
+            ObjectNode exceptionJson = objectMapper.createObjectNode();
+            exceptionJson.put("exceptionType", e.getClass().getName());
+            exceptionJson.put("code", code);
+            if (e.getMessage() != null) {
+                exceptionJson.put("error", e.getMessage());
+            }
+            return Response.status(code)
+                    .entity(exceptionJson)
+                    .build();
+        }
     }
 }
