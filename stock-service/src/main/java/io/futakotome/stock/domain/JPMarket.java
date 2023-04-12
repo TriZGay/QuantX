6accbe95c810a277064d543fe8105000990fb3e5
futakotome <v-zzz.gd@chinatelecom.cn>
2023-04-12 15:50:54 +0800

    其他市场股票数据同步

--- stock-service/src/main/java/io/futakotome/stock/domain/JPMarket.java
+++ stock-service/src/main/java/io/futakotome/stock/domain/JPMarket.java
@@ -1,12 +1,18 @@
 package io.futakotome.stock.domain;
 
 import com.futu.openapi.pb.QotCommon;
+import com.futu.openapi.pb.QotGetPlateSecurity;
 import com.futu.openapi.pb.QotGetPlateSet;
+import io.futakotome.stock.dto.PlateDto;
+import io.futakotome.stock.mapper.PlateDtoMapper;
 import io.futakotome.stock.service.QuotesService;
+import io.futakotome.stock.utils.CacheManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class JPMarket implements RequestPlateInfo {
+import java.util.List;
+
+public class JPMarket implements RequestPlateInfo, RequestStockInfo {
     private static final Logger LOGGER = LoggerFactory.getLogger(JPMarket.class);
 
     @Override
@@ -18,5 +24,32 @@ public class JPMarket implements RequestPlateInfo {
                         .build()).build();
         int seqNo = QuotesService.qot.getPlateSet(request);
         LOGGER.info("SeqNo:" + seqNo + "日本市场请求板块信息:" + request.toString());
+    }
+
+    @Override
+    public void sendStockInfoRequest(PlateDtoMapper plateDtoMapper) {
+        List<PlateDto> hkAllPlates = plateDtoMapper.searchByMarketEquals(QotCommon.QotMarket.QotMarket_JP_Security_VALUE);
+        for (int i = 0; i < hkAllPlates.size(); i++) {
+            String plateCode = hkAllPlates.get(i).getCode();
+            QotGetPlateSecurity.Request request = QotGetPlateSecurity.Request.newBuilder()
+                    .setC2S(QotGetPlateSecurity.C2S.newBuilder()
+                            .setPlate(QotCommon.Security.newBuilder()
+                                    .setMarket(QotCommon.QotMarket.QotMarket_JP_Security_VALUE)
+                                    .setCode(plateCode)
+                                    .build())
+                            .build())
+                    .build();
+            int seqNo = QuotesService.qot.getPlateSecurity(request);
+            LOGGER.info("SeqNo:" + seqNo + "日本市场请求股票信息:" + request.toString());
+            CacheManager.put(String.valueOf(seqNo), plateCode);
+            if (i != 0 && i % 9 == 0) {
+                try {
+                    LOGGER.info("接口限制每30秒最多请求10次.sleep....");
+                    Thread.sleep(30000L);
+                } catch (InterruptedException e) {
+                    LOGGER.error("sleep失败!", e);
+                }
+            }
+        }
     }
 }
