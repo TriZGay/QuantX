59b9aba35628b91155d81feabbd7f86a6752d67b
futakotome <v-zzz.gd@chinatelecom.cn>
2023-04-29 12:54:08 +0800

    板块接口

--- stock-service/src/main/java/io/futakotome/stock/controller/BaseDataController.java
+++ stock-service/src/main/java/io/futakotome/stock/controller/BaseDataController.java
@@ -4,7 +4,9 @@ import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import io.futakotome.stock.dto.PlateDto;
 import io.futakotome.stock.dto.StockDto;
+import io.futakotome.stock.mapper.PlateDtoMapper;
 import io.futakotome.stock.mapper.StockDtoMapper;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
@@ -14,16 +16,17 @@ import reactor.core.publisher.Mono;
 @RequestMapping("/base")
 public class BaseDataController {
     private final StockDtoMapper stockDtoMapper;
+    private final PlateDtoMapper plateDtoMapper;
 
-    public BaseDataController(StockDtoMapper stockDtoMapper) {
+    public BaseDataController(StockDtoMapper stockDtoMapper, PlateDtoMapper plateDtoMapper) {
         this.stockDtoMapper = stockDtoMapper;
+        this.plateDtoMapper = plateDtoMapper;
     }
 
     @PostMapping("/stocks")
     public Mono<IPage<StockDto>> listStocks(@RequestBody StockListRequest request,
                                             @RequestParam(required = false) Long page,
-                                            @RequestParam(required = false) Long limit
-    ) {
+                                            @RequestParam(required = false) Long limit) {
         QueryWrapper<StockDto> queryWrapper = Wrappers.query();
         Page<StockDto> pagination = Page.of(1, 10);
         if (request.getMarket() != null) {
@@ -35,8 +38,28 @@ public class BaseDataController {
         if (limit != null) {
             pagination.setSize(limit);
         }
-        return Mono.create(iPageFluxSink ->
-                iPageFluxSink.success(stockDtoMapper.selectPage(pagination, queryWrapper))
+        return Mono.create(iPageMonoSink ->
+                iPageMonoSink.success(stockDtoMapper.selectPage(pagination, queryWrapper))
         );
     }
+
+    @PostMapping("/plates")
+    public Mono<IPage<PlateDto>> listPlates(@RequestBody PlateListRequest plateListRequest,
+                                            @RequestParam(required = false) Long page,
+                                            @RequestParam(required = false) Long limit) {
+        QueryWrapper<PlateDto> queryWrapper = Wrappers.query();
+        Page<PlateDto> pagination = Page.of(1, 10);
+        if (plateListRequest.getMarket() != null) {
+            queryWrapper.eq("market", plateListRequest.getMarket());
+        }
+        if (page != null) {
+            pagination.setCurrent(page);
+        }
+        if (limit != null) {
+            pagination.setSize(limit);
+        }
+        return Mono.create(iPageMonoSink ->
+                iPageMonoSink.success(plateDtoMapper.selectPage(pagination, queryWrapper)));
+    }
+
 }
---
+++ stock-service/src/main/java/io/futakotome/stock/controller/PlateListRequest.java
@@ -0,0 +1,13 @@
+package io.futakotome.stock.controller;
+
+public class PlateListRequest {
+    private Integer market;
+
+    public Integer getMarket() {
+        return market;
+    }
+
+    public void setMarket(Integer market) {
+        this.market = market;
+    }
+}
