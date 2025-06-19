d1c9fbff2c34b3eb4c496847c8f6ec19d017b73e
futakotome <983528987@qq.com>
2025-06-19 20:14:02 +0800

    采集itick数据模块初始化

---
+++ 3rd-itick-service/src/main/java/io/futakotome/itick/ItickFetcherApplication.java
@@ -0,0 +1,11 @@
+package io.futakotome.itick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItickFetcherApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ItickFetcherApplication.class, args);
+    }
+}
