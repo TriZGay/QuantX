2c51933755c178d7d474af9f11cda97a7b5ec3d6
futakotome <983528987@qq.com>
2025-08-13 11:32:07 +0800

    整理代码

---
+++ sql/analysis/ema.sql
@@ -0,0 +1,37 @@
+--计算递归sql计算ema
+with recursive raw_data as (
+    select ema.market as market, ema.code as code, ema.rehab_type as rehab_type, k.close_price, ema.ema_5 as ema_5, ema.ema_10 as ema_10, ema.ema_12 as ema_12, ema.ema_20 as ema_20, ema.ema_26 as ema_26, ema.ema_60 as ema_60, ema.ema_120 as ema_120, ema.update_time as update_time
+    from t_kl_min_1_arc k inner join t_ema_min_1_arc ema on k.code=ema.code and k.rehab_type = ema.rehab_type and k.update_time = ema.update_time
+    where update_time >='2025-07-17 09:30:00' and update_time <='2025-07-17 09:32:00' and code='00700' and rehab_type =1
+    ), pre_data as (
+    select *, row_number() over ( order by update_time) as rn
+    from raw_data
+    ), ema_cte as (
+    select market, code, rehab_type, close_price, ema_5, ema_10, ema_12, ema_20, ema_26, ema_60, ema_120, update_time, rn
+    from pre_data
+    where rn = 1
+    union all
+    select p.market, p.code, p.rehab_type, p.close_price, (p.close_price*2+e.ema_5 * 4)/6 as ema_5, (p.close_price*2+e.ema_5 * 9)/11 as ema_10, (p.close_price*2+e.ema_5 * 11)/13 as ema_12, (p.close_price*2+e.ema_5 * 19)/21 as ema_20, (p.close_price*2+e.ema_5 * 25)/27 as ema_26, (p.close_price*2+e.ema_5 * 59)/61 as ema_60, (p.close_price*2+e.ema_5 * 119)/121 as ema_120, p.update_time, p.rn
+    from pre_data p, ema_cte e
+    where p.rn - 1 = e.rn
+    )
+select *
+from ema_cte;
+
+-- 按code、rehab_type分组查询最新的ema数据
+select market,
+       code,
+       rehab_type,
+       ema_5,
+       ema_10,
+       ema_12,
+       ema_20,
+       ema_26,
+       ema_60,
+       ema_120,
+       toString(update_time) as update_time
+from t_ema_min_1_arc e
+         inner join (select code, rehab_type, max(update_time) as update_time
+                     from t_ema_min_1_arc
+                     group by (code, rehab_type)) e2
+                    on e.code = e2.code and e.rehab_type = e2.rehab_type and e.update_time = e2.update_time;
